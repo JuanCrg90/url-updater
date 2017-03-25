4d1cda0cf212e57364ae97fa8afc53c3e26b657c
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2017-03-25 12:47:07 -0600

    Add Search and replace in File and unziped directory

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
-
 var program = require('commander');
+var searchAndReplace = require('./lib/utils').searchAndReplace;
 
 program
   .arguments('<file>')
   .option('-s, --search <search-url>', 'URL to seach')
   .option('-r, --replace <replace-url>', 'New URL')
   .action(function(file) {
-    console.log('search-url: %s replace-url: %s file: %s',
-      program.search, program.replace, file);
+    searchAndReplace(program.search, program.replace, file)
   })
   .parse(process.argv);
