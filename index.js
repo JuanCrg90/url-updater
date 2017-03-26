4814401168690c61100718d867491016a74e8bd5
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2017-03-25 19:19:14 -0600

    Add Zip Support

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 #!/usr/bin/env node
 var program = require('commander');
 var searchAndReplace = require('./lib/utils').searchAndReplace;
+var isCompressed = require('./lib/utils').isCompressed;
+var removeExtension = require('./lib/utils').removeExtension;
+var removeUnzipped = require('./lib/utils').removeUnzipped;
+var unzipFile = require('./lib/unzipFile');
+var zipFile = require('./lib/zipFile');
 
 program
-  .arguments('<file>')
+  .arguments('<filePath>')
   .option('-s, --search <search-url>', 'URL to seach')
   .option('-r, --replace <replace-url>', 'New URL')
-  .action(function(file) {
-    searchAndReplace(program.search, program.replace, file)
+  .action(function(filePath) {
+    if(isCompressed(filePath)) {
+      var filePathNoExtension = removeExtension(filePath);
+      unzipFile(filePath);
+      searchAndReplace(program.search, program.replace, filePathNoExtension);
+      zipFile(filePathNoExtension);
+      removeUnzipped(filePathNoExtension);
+    } else {
+      console.log('Replacing URL');
+      searchAndReplace(program.search, program.replace, filePath);
+    }
+
+      console.log('Done');
   })
   .parse(process.argv);
